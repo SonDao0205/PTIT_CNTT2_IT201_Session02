ceb59013f302beeb7982005e690af4ca837585ad
Dao Truong Son <daotruongson2006@gmail.com>
2025-06-17 15:31:48 +0700

    submit

--- PTIT_CNTT2_IT201_Session02_Bai10.c
+++ PTIT_CNTT2_IT201_Session02_Bai10.c
@@ -1,9 +1,26 @@
 //
 // Created by SonwDao on 17/6/25.
 //
-#include"stdio.h
-#include"stdlib.h"
+#include<stdio.h>
+#include<stdlib.h>
 void countExist(int array[],int n) {
+    int exist[n];
+    for(int i=0;i<n;i++) {
+        exist[i]=0;
+    }
+    for(int i=0;i<n;i++) {
+        if(exist[i] == 1) {
+            continue;
+        }
+        int count = 1;
+        for (int j = i+1; j < n; ++j) {
+            if(array[i] == array[j]) {
+                exist[j] = 1;
+                count++;
+            }
+        }
+        printf("Phần tử %d xuất hiện %d lần\n",array[i],count);
+    }
 }
 
 int main() {
@@ -19,6 +36,7 @@ int main() {
         printf("Nhập array[%d] : ", i);
         scanf("%d", &array[i]);
     }
+    countExist(array, n);
     free(array);
     return 0;
 }
